0300aae40d06fcccc3199a64b70612a3c55682e5
mengsea <ymengsea@gmail.com>
2024-06-18 10:49:28 +0700

    done building UI for the app status feature , in scenebuilder

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -1,15 +1,22 @@
 package App;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-    public class App extends Application {
+import java.io.IOException;
+
+public class App extends Application {
         @Override
-        public void start(Stage window){
+        public void start(Stage window)throws IOException {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("App.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
             Image icon = new Image("App/apple.png");
             window.getIcons().add(icon);
             window.setTitle("ShopManagement");
+            window.setScene(scene);
             window.show();
         }
 
